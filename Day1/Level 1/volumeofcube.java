5b0af66fec150eea57df1f305c89ac52b0c26f4b
Ankit0427 <153299876+Ankit0427@users.noreply.github.com>
2025-01-10 16:01:54 +0530

    Delete Day1 directory

--- Day1/Level 1/volumeofcube.java
+++
@@ -1,9 +0,0 @@
-public class volumeofcube{
- 	
-	public static void main(String[] args)
-{
-	int radius=6378 ;
-	double area= ( 4 * Math.PI * Math.pow(radius,3) ) /3;
-	System.out.println(" The volume of earth in cubic kilometers is " + area + " and cubic miles is " + area*0.239913 );
-}
-}
