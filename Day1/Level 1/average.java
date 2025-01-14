a53e02be737cfc14591cbabb22c2c5a248b08caf
Ankit0427 <153299876+Ankit0427@users.noreply.github.com>
2025-01-14 10:23:39 +0530

    Day 3 solved

--- Day1/Level 1/average.java
+++
@@ -1,9 +0,0 @@
-public class average {
-    public static void main(String[] args) {
-        
-        int average = (94+95+96)/3;
-
-        System.out.println("Sam's average mark in PCM is " + average);
-
-    }
-}
--- Day1/Level 1/pens.java
+++
@@ -1,8 +0,0 @@
-public class pens {
-    public static void main(String[] args) {
-        int total_pen=14;
-        int students=3;
-
-        System.out.println("The Pen Per Student is " + (total_pen/students) + " and the remaining pen not distributed is " + (total_pen%students));
-    }
-}
